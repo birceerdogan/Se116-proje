5a6cd03b013dee5716145f2ea8dd1c6fabc148e5
Barisahin35 <barssahn35@gmail.com>
2025-04-29 15:47:08 +0300

    We do Hashmap and Hashsets

--- src/FSMMain.java
+++ src/FSMMain.java
@@ -5,6 +5,16 @@ import java.util.Map;
 import java.util.Set;
 
 public class FSMMain {
+    private static final String VERSION = "1.0";
+    private static Set<Character> symbols = new HashSet<>();
+    private static Set<String> states = new HashSet<>();
+    private static String initialState = null;
+    private static Set<String> finalStates = new HashSet<>();
+    private static Map<String, Map<Character, String>> transitions = new HashMap<>();
+    private static PrintWriter logWriter = null;
+    private static boolean loggingEnabled = false;
+    private static String logFileName = "";
+
     private static class FSMSerializable implements Serializable {
         private static final long serialVersionUID = 1L;
         private Set<Character> symbols;
