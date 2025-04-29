c3e830e7cad67667fa565cc9b094452c64a39736
birceerdogan <154711872+birceerdogan@users.noreply.github.com>
2025-04-29 14:10:19 +0300

    our FSMSerializable static class and their necessary things

---
+++ src/FSMMain.java
@@ -0,0 +1,37 @@
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class FSMMain {
+    private static class FSMSerializable implements Serializable {
+        private static final long serialVersionUID = 1L;
+        private Set<Character> symbols;
+        private Set<String> states;
+        private String initialState;
+        private Set<String> finalStates;
+        private Map<String, Map<Character, String>> transitions;
+
+        public FSMSerializable(Set<Character> symbols, Set<String> states, String initialState,
+                               Set<String> finalStates, Map<String, Map<Character, String>> transitions) {
+            this.symbols = new HashSet<>(symbols);
+            this.states = new HashSet<>(states);
+            this.initialState = initialState;
+            this.finalStates = new HashSet<>(finalStates);
+            this.transitions = new HashMap<>(transitions);
+        }
+
+        public Set<Character> getSymbols() {
+            return symbols; }
+        public Set<String> getStates() {
+            return states; }
+        public String getInitialState() {
+            return initialState; }
+        public Set<String> getFinalStates() {
+            return finalStates; }
+        public Map<String, Map<Character, String>> getTransitions() {
+            return transitions; }
+    }
+}
+
