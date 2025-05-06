ccf8e1d1595a01415433d54755f92d7e33fd4bbf
bellaxx <sudesavas_@hotmail.com>
2025-05-06 15:42:23 +0300

    handleCompıle metot

--- src/FSMMain.java
+++ src/FSMMain.java
@@ -5,7 +5,20 @@ import java.time.*;
 
 public class FSMMain {
 
+    private static void handleCompile(String args) {
+        if (args.isEmpty()) {
+            System.out.println("Error: No filename specified");
+            return;
+        }
 
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(args))) {
+            FSMSerializable fsm = new FSMSerializable(symbols, states, initialState, finalStates, transitions);
+            oos.writeObject(fsm);
+            System.out.println("Compile successful");
+        } catch (IOException e) {
+            System.out.println("Error compiling to file '" + args + "': " + e.getMessage());
+        }
+    }
     private static void handleClear() {
         symbols.clear();
         states.clear();
